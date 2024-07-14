4b68387c3529f0f11bf5ffd4fd928a50d3dd68ed
NTL212 <loiloicong121@gmail.com>
2024-07-14 12:19:50 +0700

    Update comfirm modal

---
+++ OnlineShop/wwwroot/assets/js/modal_comfirm.js
@@ -0,0 +1,48 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('orderForm');
+    const modal = document.getElementById('modal_confirm');
+    const closeBtn = document.getElementsByClassName('modal_confirm-close-btn')[0];
+    const confirmBtn = document.getElementById('confirmBtn');
+    const cancelBtn = document.getElementById('cancelBtn');
+    const cancelOrderLink = document.getElementById('cancelOrderLink');
+    let href;
+    if (form != null) {
+        form.onsubmit = function (event) {
+            event.preventDefault();
+            modal.style.display = 'block';
+        }
+
+        confirmBtn.onclick = function () {
+            form.submit();
+            modal.style.display = 'none';
+        }
+
+    }
+    
+    if (cancelOrderLink != null) {
+        cancelOrderLink.onclick = function (event) {
+            event.preventDefault();
+            href = cancelOrderLink.getAttribute('href');
+            modal.style.display = 'block';
+        }
+
+        confirmBtn.onclick = function () {
+            window.location.href = href;
+            modal.style.display = 'none';
+        }
+    }
+    closeBtn.onclick = function () {
+        modal.style.display = 'none';
+    }
+
+   
+    cancelBtn.onclick = function () {
+        modal.style.display = 'none';
+    }
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
+});
